90f67f9c0832e0055cd92c00048384bc525f109a
kusumashivanna <kusumashivanna98@gmail.com>
2019-08-22 15:49:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Newsfeedcontainer from "./container/Newsfeedcontainer";
+
+export default class App extends Component {
+  render() {
+    return <Newsfeedcontainer />;
+  }
+}
